c02b699a7f97cea546f66d50106fae235dd6c9cf
Sarthak <sarthakkaner7777@gmail.com>
2023-08-31 20:41:58 +0530

    calculated actual value and money value

--- MT23081_assignment1.cpp
+++ MT23081_assignment1.cpp
@@ -7,6 +7,39 @@
 
 using namespace std;
 
+double calculateActualValue(double amount, int year, double interest_rates[])
+{
+    int current_year = 2022;
+    int prev_year = 1960;
+    int index_year = year - prev_year;
+
+    double actual_value = amount;
+    double net_rate;
+    for (int i = index_year; i <= (current_year - prev_year); i++)
+    {
+        net_rate = interest_rates[i];
+        actual_value *= (1 + (net_rate / 100));
+    }
+
+    return actual_value;
+}
+
+double calculateMoneyValue(double amount, int year, double inflation_rates[], double interest_rates[])
+{
+    int current_year = 2022;
+    int prev_year = 1960;
+    int index_year = year - prev_year;
+
+    double money_value = amount;
+    double net_rate;
+
+    for (int i = index_year; i <= (current_year - prev_year); i++)
+    {
+        net_rate = interest_rates[i] - inflation_rates[i];
+        money_value *= (1 + (net_rate / 100));
+    }
+    return money_value;
+}
 
 int main()
 {
@@ -83,7 +116,12 @@ int main()
         return 1;
     }
 
-   
+    actual_value = calculateActualValue(amount, year, intrestRate);
+    money_value = calculateMoneyValue(amount, year, inflationRate, intrestRate);
+
+    cout << fixed << setprecision(2);
+    cout << "actual money received in 2022: " << actual_value << endl;
+    cout << "value of money in 2022: " << money_value << endl;
 
     return 0;
 }
